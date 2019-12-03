79fb2c0c4d8dc493ab48002427c5e50d430b4c6e
sj602 <tjswls602@gmail.com>
2019-12-03 15:21:45 +0900

    eb setting

--- KiriKini/KiriKini/settings.py
+++ KiriKini/KiriKini/settings.py
@@ -156,16 +156,39 @@ WSGI_APPLICATION = 'KiriKini.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'kirikini',
-        'USER': 'admin',
-        'PASSWORD': 'admin', # DEV: admin, PROD: get_secret("DATABASE_PASSWORD")
-        'HOST': 'localhost', # DEV: localhost, PROD: ec2-54-180-8-109.ap-northeast-2.compute.amazonaws.com
-        'PORT': '',
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'kirikini',
+            'USER': 'admin',
+            'PASSWORD': 'admin',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'kirikini',
+#         'USER': 'admin',
+#         'PASSWORD': 'admin', # DEV: admin, PROD: get_secret("DATABASE_PASSWORD")
+#         'HOST': 'localhost', # DEV: localhost, PROD: ec2-54-180-8-109.ap-northeast-2.compute.amazonaws.com
+#         'PORT': '',
+#     }
+# }
 
 
 # Password validation
