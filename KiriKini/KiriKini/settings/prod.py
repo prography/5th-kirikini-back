c3db9f329ac0d76da9cdd906a446f71917cb2234
sj602 <tjswls602@gmail.com>
2020-03-27 17:28:28 +0900

    requirements-dev.txt

--- KiriKini/KiriKini/settings/prod.py
+++ KiriKini/KiriKini/settings/prod.py
@@ -7,9 +7,9 @@ ALLOWED_HOSTS = ['13.124.158.62']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': 'kirikini-db.cyb55qlklqs4.ap-northeast-2.rds.amazonaws.com',
+        'HOST': 'kirikini.cyb55qlklqs4.ap-northeast-2.rds.amazonaws.com',
         'PORT': '5432',
-        'NAME': 'kirikini-db',
+        'NAME': 'kirikini',
         'USER': get_secret("RDS_USER"),
         'PASSWORD': get_secret("RDS_PASSWORD"),
     }
